3d8cb43a1fe0a38305091092515e88cadf120938
mnunezsa <mnunezsa95@gmail.com>
2023-12-11 14:05:00 -0500

    update

--- prep.js
+++ prep.js
@@ -1,6 +1,67 @@
-/* Q1: Write a JavaScript function to calculate the sum of two numbers. */
+// ! Q1: Write a JavaScript function to calculate the sum of two numbers.
+// * create a function that takes in two numbers as inputs and returns the sum
 
-// create a function that takes in two numbers as inputs and returns the sum
 const addTwoNumbers = (a, b) => {
   return a + b;
 };
+
+const value1 = addTwoNumbers(10, 15);
+console.log(value1);
+
+// =======================
+
+// ! Q2: Write a JavaScript program to find the maximum number in an array.
+// * create a fn that takes an array as an input; Math.max() function will find largest number
+
+const findMaxNumber = (arr) => {
+  return Math.max(...arr); // the spread operator is used to unpack the array
+};
+
+const value2 = findMaxNumber([1, 2, 3, 4, 5]);
+console.log(value2);
+
+// =======================
+
+// ! Q3: Write a JavaScript function to check if a given string is a palindrome (reads the same forwards and backwards).
+// * create a function that takes a str as an input. This function checks if the input matches a reversed input.
+// * the reverse() method reverses the order of an array
+
+function isPalindrome(str) {
+  const reversedStr = str.split("").reverse().join("");
+  if (str === reversedStr) {
+    return true;
+  }
+}
+
+const value3 = isPalindrome("stanley yelnats");
+console.log(value3);
+
+// =======================
+
+// ! Q4: Write a JavaScript program to reverse a given string
+// * same process as above
+
+function reverseString(str) {
+  const reversedStr = str.split("").reverse().join("");
+  return reversedStr;
+}
+const value4 = reverseString("hello");
+console.log(value4);
+
+// =======================
+
+// ! Q5: Write a JavaScript function that takes an array of numbers and returns a new array with only the even numbers
+// * create a fn that takes an array as an input, checks if evenly divisible by 2 and returns only ones that are
+
+const isEven = (arr) => {
+  const evenNumbers = [];
+  for (let i = 0; i <= arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      evenNumbers.push(arr[i]);
+    }
+  }
+  return evenNumbers;
+};
+
+const value5 = isEven([1, 2, 3, 4, 5]);
+console.log(value5);
