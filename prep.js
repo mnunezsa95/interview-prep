b6d0e9887f4eaf8d556042a005832c735c1776d1
mnunezsa <mnunezsa95@gmail.com>
2023-12-11 16:28:42 -0500

    update

--- prep.js
+++ prep.js
@@ -65,3 +65,35 @@ const isEven = (arr) => {
 
 const value5 = isEven([1, 2, 3, 4, 5]);
 console.log(value5);
+
+// =======================
+
+// ! Q6: Write a JavaScript program to calculate the factorial of a given number
+// * create a function that takes in a number, and multiples it by every number that comes before it
+
+const getFactorial = (num) => {
+  let res = 1;
+  for (let i = num; i >= 1; i--) {
+    res = res * i;
+  }
+  return res;
+};
+
+const value6 = getFactorial(5);
+console.log(value6);
+
+// ! Q7: Write a JavaScript function to check if a given number is prime.
+// * create a fn that takes an number as an input; loop from 2 to the square root of the number; if a number evenly divides it, it is not prime
+
+const isPrime = (num) => {
+  if (num <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const value7 = isPrime(5);
+console.log(value7);
