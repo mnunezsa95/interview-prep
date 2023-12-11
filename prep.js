cdef7ff2470eb94f5aa4bfd472d3cf4d742e866e
mnunezsa <mnunezsa95@gmail.com>
2023-12-11 13:37:59 -0500

    update

---
+++ prep.js
@@ -0,0 +1,6 @@
+/* Q1: Write a JavaScript function to calculate the sum of two numbers. */
+
+// create a function that takes in two numbers as inputs and returns the sum
+const addTwoNumbers = (a, b) => {
+  return a + b;
+};
