005be3b1e70818118855c28329924594daae3971
mnunezsa <mnunezsa95@gmail.com>
2023-12-21 14:12:31 -0600

    update

--- codewarsExercises.js
+++ codewarsExercises.js
@@ -629,3 +629,125 @@ console.log(powerOf4(8));
 console.log(powerOf4(16));
 console.log(powerOf4(1));
 console.log(powerOf4(4));
+
+/* ---------------------------------------------------------------------------------------------- */
+/*                                Question 24: Blue and red marbles                               */
+/* ---------------------------------------------------------------------------------------------- */
+/*
+You've decided to write a function, guessBlue() to help automatically calculate whether you should guess 
+"blue" or "red". The function should take four arguments:
+- the number of blue marbles you put in the bag to start
+- the number of red marbles you put in the bag to start
+- the number of blue marbles pulled out so far (always lower than the starting number of blue marbles)
+- the number of red marbles pulled out so far (always lower than the starting number of red marbles)
+
+guessBlue() should return the probability of drawing a blue marble, expressed as a float. For example, 
+guessBlue(5, 5, 2, 3) should return 0.6.
+*/
+
+function guessBlue(blueStart, redStart, bluePulled, redPulled) {
+  let blueRemaining = blueStart - bluePulled;
+  let redRemaining = redStart - redPulled;
+  return +(blueRemaining / (blueRemaining + redRemaining));
+}
+
+console.log(guessBlue(5, 5, 2, 3)); // Output 0.6
+
+/* ---------------------------------------------------------------------------------------------- */
+/*                                   Question 25: Average Array                                   */
+/* ---------------------------------------------------------------------------------------------- */
+/* 
+Create a function that takes a 2D array as an input, and outputs another array that contains the average 
+values for the numbers in the nested arrays at the corresponding indexes.
+
+Examples:
+    [ [1, 2, 3, 4], [5, 6, 7, 8] ]  ==>  [3, 4, 5, 6]
+
+    1st array: [1, 2, 3, 4]
+    2nd array: [5, 6, 7, 8]
+                |  |  |  |
+                v  v  v  v
+    average:   [3, 4, 5, 6]
+
+    And another one:
+    [ [2, 3, 9, 10, 7], [12, 6, 89, 45, 3], [9, 12, 56, 10, 34], [67, 23, 1, 88, 34] ]
+    average: ==>  [22.5, 11, 38.75, 38.25, 19.5]
+
+    1st array: [  2,   3,    9,   10,    7]
+    2nd array: [ 12,   6,   89,   45,    3]
+    3rd array: [  9,  12,   56,   10,   34]
+    4th array: [ 67,  23,    1,   88,   34]
+                  |    |     |     |     |
+                  v    v     v     v     v
+    average:   [22.5, 11, 38.75, 38.25, 19.5]
+
+Note: the function should also work with negative numbers and floats.
+*/
+
+const avgArray = (arr) => {
+  // create a copy of the first array in the sequence & fill it with zero
+  const initialArray = Array.from({ length: arr[0].length }, () => {
+    return 0;
+  });
+
+  const totalValues = arr.reduce((result, arr) => {
+    arr.forEach((value, index) => {
+      return (result[index] += value);
+    });
+    return result;
+  }, initialArray);
+
+  // take ever value in the new added array and divide by length of the array
+  const totalAverages = totalValues.map((value) => value / arr.length);
+  return totalAverages;
+};
+
+console.log(
+  avgArray([
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+  ])
+);
+
+// solution 2
+function avgArray2(arr) {
+  let ar = [];
+  for (let i = 0; i < arr[0].length; ++i) {
+    let sum = 0;
+    for (let j = 0; j < arr.length; ++j) {
+      sum += arr[j][i];
+    }
+    ar.push(sum / arr.length);
+  }
+  return ar;
+}
+console.log(
+  avgArray2([
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+  ])
+);
+
+/* ---------------------------------------------------------------------------------------------- */
+/*                                  Question 26: Acrostic Reader                                  */
+/* ---------------------------------------------------------------------------------------------- */
+
+/*
+An acrostic is a text in which the first letter of each line spells out a word. It is also a quick and 
+cheap way of writing a poem for somebody, as exemplified below:
+
+Write a program that reads an acrostic to identify the "hidden" word. Specifically, your program will 
+receive a list of words (reprensenting an acrostic) and will need to return a string corresponding to 
+the word that is spelled out by taking the first letter of each word in the acrostic.
+*/
+
+function readOut(acrostic) {
+  let res = "";
+  console.log(acrostic);
+  acrostic.forEach((word, i, arr) => {
+    return (res += word[0]);
+  });
+  return res;
+}
+
+console.log(readOut(["Jolly", "Amazing", "Courteous", "Keen"])); // Jack
