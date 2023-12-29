c1209ca7f793ecd9aba3c6405d48bf6562696f3b
mnunezsa <mnunezsa95@gmail.com>
2023-12-29 09:48:35 -0600

    update

--- codewarsExercises.js
+++ codewarsExercises.js
@@ -1029,3 +1029,169 @@ function removeQ2(string) {
 
 // console.log(removeQ2("hi!"));
 console.log(removeQ2("!!!Hi !!hi!!! !hi"));
+
+/* ---------------------------------------------------------------------------------------------- */
+/*                                Question 37: Duplicate sandwhich                                */
+/* ---------------------------------------------------------------------------------------------- */
+
+/*
+In this kata you will be given a list consisting of unique elements except for one thing that appears 
+twice.
+
+Your task is to output a list of everything inbetween both occurrences of this element in the list.
+
+[0, 1, 2, 3, 4, 5, 6, 1, 7, 8] => [2, 3, 4, 5, 6]
+["None", "Hello", "Example", "hello", "None", "Extra"] => ["Hello", "Example", "hello"]
+[0, 0] => []
+[true, false, true] => [false]
+"example" => "xampl"
+
+Notes:
+- All elements will be the same datatype.
+- All used elements will be primitive.
+*/
+
+function duplicateSandwich(a) {
+  const appeared = [];
+  for (let i = 0; i < a.length; i++) {
+    const item = a[i];
+    if (!appeared.includes(item)) {
+      appeared.push(item);
+    } else {
+      const startIndex = a.indexOf(item) + 1;
+      const endIndex = i;
+      return a.slice(startIndex, endIndex);
+    }
+  }
+  // If no duplicate is found, return an empty array or handle it as needed
+  return [];
+}
+
+console.log(duplicateSandwich([0, 1, 2, 3, 4, 5, 6, 1, 7, 8]));
+console.log(duplicateSandwich("example"));
+
+/* ---------------------------------------------------------------------------------------------- */
+/*                                   Question 38: Shortest Word                                   */
+/* ---------------------------------------------------------------------------------------------- */
+/*
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+*/
+
+function findShort(s) {
+  const newS = s.split(" ");
+  let shortest = newS[0].length;
+  for (let i = 0; i < newS.length; i++) {
+    if (newS[i].length < shortest) {
+      shortest = newS[i].length;
+    }
+  }
+  return shortest;
+}
+
+console.log(findShort("bitcoin take over the world maybe who knows perhaps"));
+
+/* ---------------------------------------------------------------------------------------------- */
+/*                         Question 39: Multiply all elements in an array                         */
+/* ---------------------------------------------------------------------------------------------- */
+
+/*
+To complete this Kata you need to make a function multiplyAll/multiply_all which takes an array of 
+integers as an argument. This function must return another function, which takes a single integer as 
+an argument and returns a new array.
+
+The returned array should consist of each of the elements from the first array multiplied by the integer.
+
+Example:
+multiplyAll([1, 2, 3])(2) = [2, 4, 6];
+
+You must not mutate the original array.
+*/
+
+// * create a function that takes arr as an arguement
+function multiplyAll(arr) {
+  // * declare a function that takes an arguement
+  function multiply(int) {
+    // * map each element in the array and multiply each element by the integer
+    return arr.map((item) => {
+      return item * int;
+    });
+  }
+  return multiply;
+}
+
+console.log(multiplyAll([1, 2, 3])(2));
+
+/* ---------------------------------------------------------------------------------------------- */
+/*              Question 40: Count the number of exclamation marks and question marks             */
+/* ---------------------------------------------------------------------------------------------- */
+
+/*
+Count the number of exclamation marks and question marks, return the product.
+
+""          --->   0
+"!"         --->   0
+"!ab? ?"    --->   2
+"!!"        --->   0
+"!??"       --->   2
+"!???"      --->   3
+"!!!??"     --->   6
+"!!!???"    --->   9
+"!???!!"    --->   9
+"!????!!!?" --->  20
+*/
+
+function product(string) {
+  let questionMark = 0;
+  let exclamationMark = 0;
+
+  if (!string) return 0;
+  string.split("").forEach((item) => {
+    if (item === "!") {
+      exclamationMark += 1;
+    } else if (item === "?") {
+      questionMark += 1;
+    }
+  });
+  return questionMark * exclamationMark;
+}
+
+console.log(product("!????!!!?"));
+console.log(product(""));
+
+/* ---------------------------------------------------------------------------------------------- */
+/*                                 Question 41: Easy Time Convert                                 */
+/* ---------------------------------------------------------------------------------------------- */
+/*
+This kata requires you to convert minutes (int) to hours and minutes in the format hh:mm (string).
+
+If the input is 0 or negative value, then you should return "00:00"
+
+Hint: use the modulo operation to solve this challenge. The modulo operation simply returns the remainder 
+after a division. For example the remainder of 5 / 2 is 1, so 5 modulo 2 is 1.
+
+Example
+If the input is 78, then you should return "01:18", because 78 minutes converts to 1 hour and 18 minutes.
+*/
+
+function timeConvert(num) {
+  if (num === 0 || num < 0) return "00:00";
+  let hours = Math.floor(num / 60);
+  let mins = Math.floor(num % 60);
+
+  if (hours < 10 && mins < 10) {
+    return `0${hours}:0${mins}`;
+  } else if (hours < 10) {
+    return `0${hours}:${mins}`;
+  } else if (mins < 10) {
+    return `${hours}:0${mins}`;
+  } else {
+    return `${hours}:${mins}`;
+  }
+}
+
+console.log(timeConvert(0));
+console.log(timeConvert(-6));
+console.log(timeConvert(75));
+console.log(timeConvert(970));
