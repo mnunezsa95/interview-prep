d7d84f09308e7c17a545180b1434727e1c481e8a
mnunezsa <mnunezsa95@gmail.com>
2024-01-15 17:53:05 -0500

    update

---
+++ codewars_exercises/67_splitInParts.js
@@ -0,0 +1,34 @@
+/* ---------------------------------------------------------------------------------------------- */
+/*                                         Split in Parts                                         */
+/* ---------------------------------------------------------------------------------------------- */
+
+/*
+Difficulty: 7 kyu
+
+The aim of this kata is to split a given string into different strings of equal size (note size of strings 
+is passed to the method)
+
+Example:
+-Split the below string into other strings of size #3
+'supercalifragilisticexpialidocious'
+
+-Will return a new string
+--'sup erc ali fra gil ist ice xpi ali doc iou s'
+
+Assumptions:
+-String length is always greater than 0
+-String has no spaces
+-Size is always positive
+*/
+
+var splitInParts = function (s, partLength) {
+  let array = [];
+  for (let i = 0; i < s.length; i += partLength) {
+    array.push(s.slice(i, i + partLength));
+  }
+  return array.join(" ");
+};
+
+console.log(splitInParts("supercalifragilisticexpialidocious", 3));
+console.log(splitInParts("HelloKata", 1));
+console.log("HelloKata");
