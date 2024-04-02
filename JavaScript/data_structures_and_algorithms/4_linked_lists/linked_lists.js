4ffb6dda0cc84be1fdeefcba266c0f7e7952b359
mnunezsa <mnunezsa95@gmail.com>
2024-04-02 17:20:17 -0400

    updating LinkedLists notes

---
+++ JavaScript/data_structures_and_algorithms/4_linked_lists/linked_lists.js
@@ -0,0 +1,93 @@
+/* ---------------------------------------------------------------------------------------------- */
+/*                                    Linked Lists: Constructor                                   */
+/* ---------------------------------------------------------------------------------------------- */
+
+/*
+The 'Node' class creates a new Node based on the value passed into it
+- Sets the value equal to the value passed in at initalization
+- Sets the pointer equal to null at initalization
+*/
+
+/*
+The 'LinkedList' class creates a new LinkedList
+- The 'LinkedList' class takes in the value and creates a new Node
+*/
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    const newNode = new Node(value); // Creates a new instance of the Node class with the value it recieved
+    this.head = newNode; // Sets the head of list as the new Node
+    this.tail = this.head; // Has the tail pointing to the head
+    this.length = 1;
+  }
+
+  /* 
+  push() is O(1) operation
+  Edge Cases:
+  - 1) The node does not have any nodes yet
+  */
+  push(value) {
+    const newNode = new Node(value); // Creates a new instance of the Node class with the value it recieved from the push fn
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  /* 
+  pop() is O(n) operation
+  Edge Cases:
+  - 1) A LL that does not have any items
+  - 2) A LL that only has 1 item
+  */
+  pop() {
+    if (!this.head) return undefined; // return undefined if there are no nodes
+    let temp = this.head;
+    let pre = this.head;
+    while (temp.next) {
+      pre = temp;
+      temp = temp.next;
+    }
+    this.tail = pre;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      // Set head and tail to null if the length of the LL is 0
+      this.head = null;
+      this.tail = null;
+    }
+    return temp; // Return the number that was popped off
+  }
+
+  unshift(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+let myLinkedList = new LinkedList(11); // Creating a linkedlist with a value of 4
+myLinkedList.push(3);
+myLinkedList.push(23);
+myLinkedList.push(7);
+
+console.log(myLinkedList);
--- JavaScript/data_structures_and_algorithms/4_linked_lists/linked_lists_notes.md
+++ JavaScript/data_structures_and_algorithms/4_linked_lists/linked_lists_notes.md
@@ -10,10 +10,16 @@
   - Linked Lists have a head (points to first item)
   - Linked Lists have a tail (points to the last item)
   - Each item in the linked list points to the nex item, then to the next,
-  - The last item points to null (null-terminated)
+  - The last item points to null (also called null-terminated list)
 - Arrays are in contigous places in memory
+- Linked lists can be all over the place in memory
 
-## Big O of Linked Lists & Arrays
+## Big O of Linked Lists
+
+- Looking up value or looking by index is O(n)
+- Adding and removing from the beginning of a Linked List is O(1)
+
+## Using a Linked List vs an Array
 
 - When to use a Linked List vs an Array
   - Adding and removing items (shift & unshift) from the front of a list are better for linked lists
